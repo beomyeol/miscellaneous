e26abef92711ed18f0ac13b4f5a226aa653f199b
Beomyeol Jeon <beomyeolj@gmail.com>
2017-02-15 10:53:30 -0700

    Complete the script for installing hadoop

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -2,23 +2,32 @@
 
 HADOOP_URL=http://apache.cs.utah.edu/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
 TEMP_DIR=/tmp/hadoop
+HADOOP_VERSION=2.7.3
+HADOOP_BINARY_ARCHIVE=hadoop-${HADOOP_VERSION}.tar.gz
+HADOOP_HOME=/opt/hadoop
 
 # install necesary package
 sudo apt-get install wget
 
-# download hadoop binary archive
-if [ ! -e hadoop-2.7.3.tar.gz ] then
-  wget $HADOOP_URL
+if [ ! -d $TEMP_DIR ]; then
+  mkdir -p /tmp/hadoop
 fi
 
-mkdir -p /tmp/hadoop
-cp hadoop-2.7.3.tar.gz /tmp/hadoop
+if [ ! -e $TEMP_DIR/$HADOOP_BINARY_ARCHIVE ]; then
+  # download hadoop binary archive
+  if [ ! -e $HADOOP_BINARY_ARCHIVE ]; then
+    wget $HADOOP_URL
+  fi
+  cp $HADOOP_BINARY_ARCHIVE $TEMP_DIR
+fi
 
-pushd /tmp/hadoop
+pushd $TEMP_DIR
 
-tar xzf hadoop-2.7.3.tar.gz
-mv hadoop-2.7.3 hadoop
+tar xzf $HADOOP_BINARY_ARCHIVE
+mv hadoop-$HADOOP_VERSION hadoop
+sudo mv hadoop /opt
 
-pushd /tmp
+echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.bashrc
+echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.zshrc
 
 popd
