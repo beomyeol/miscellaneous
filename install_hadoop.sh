69327fa1cb3d6628bdc25bd3bde4c87d517b35b5
Beomyeol Jeon <beomyeolj@gmail.com>
2017-02-15 14:21:27 -0700

    Fix

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 HADOOP_URL=http://apache.cs.utah.edu/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
 TEMP_DIR=/tmp/hadoop
@@ -7,7 +7,8 @@ HADOOP_BINARY_ARCHIVE=hadoop-${HADOOP_VERSION}.tar.gz
 HADOOP_HOME=/opt/hadoop
 
 # install necesary package
-sudo apt-get install wget
+sudo apt-get update
+sudo apt-get install wget openjdk-7-jre -y
 
 if [ ! -d $TEMP_DIR ]; then
   mkdir -p /tmp/hadoop
@@ -27,7 +28,18 @@ tar xzf $HADOOP_BINARY_ARCHIVE
 mv hadoop-$HADOOP_VERSION hadoop
 sudo mv hadoop /opt
 
-echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.bashrc
-echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.zshrc
-
 popd
+
+# Set environment variables
+
+#echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
+#echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.zshrc
+#
+#echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.bashrc
+#echo "export HADOOP_HOME=$HADOOP_HOME" >> ~/.zshrc
+#
+#echo "export PATH=${HADOOP_HOME}/bin:$PATH" >> ~/.bashrc
+#echo "export PATH=${HADOOP_HOME}/bin:$PATH" >> ~/.zshrc
+#
+#echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> ~/.bashrc
+#echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> ~/.zshrc
