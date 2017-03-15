80085a5e7ccad9262e1d47623dd9508400eae2e9
Beomyeol Jeon <beomyeolj@gmail.com>
2017-03-15 17:20:37 -0500

    Add nvidia-docker install script

---
+++ scripts/install_nvidia-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install docker-ce
+
+# Remove old packages if exsits
+sudo apt-get remove docker docker-engine
+
+sudo apt-get install \
+             apt-transport-https \
+             ca-certificates \
+             curl \
+             software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+# Install nvidia-driver
+sudo apt-get install -y nvidia-367-dev
+
+# Install nvidia-docker and nvidia-docker-plugin
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
